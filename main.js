68dd3c4ab0a8d6d3cc7db020a9492cdf9ef28bb2
behr-ipk <48517774+behr-ipk@users.noreply.github.com>
2019-03-13 17:41:09 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+let values = getUrlVars();
+
+function getUrlVars() {
+    var vars = {};
+     parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+let allValues = document.getElementsByTagName("*");
+let url = new URL(window.location.href)
+
+for (val in allValues){
+
+
+   let tempVal = allValues[val].className
+    //console.log(typeof tempVal)
+   if (typeof tempVal !="undefined"&& tempVal !="") {
+   
+
+    let currentText = $(`.${tempVal}`).text();
+    $(`.${tempVal}`).text(`${currentText} ${url.searchParams.get(tempVal)}`)
+
+   }
+
+}
+//console.log(allValues[10].className)
+
+    
+})
